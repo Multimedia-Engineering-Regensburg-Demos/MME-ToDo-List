f644088dd3bfa38642d729a0be53496b4b74a9c4
Alexander Bazo <alexanderbazo@googlemail.com>
2019-04-23 12:52:34 +0200

    Add solution

---
+++ resources/js/Task.js
@@ -0,0 +1,23 @@
+/* eslint-env browser */
+
+class Task {
+
+  constructor(description, id, completed) {
+    this.description = description || "New Task";
+    this.id = id || Date.now().toString();
+    this.completed = completed || false;
+    this.listeners = {};
+  }
+  
+  setDescription(description) {
+    this.description = description;
+  }
+
+  toggleStatus() {
+    this.completed = !this.completed;
+    return this.completed;
+  }
+
+}
+
+export default Task;
