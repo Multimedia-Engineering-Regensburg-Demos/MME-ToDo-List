d7aecd0fcfd665b62eeb013b567d26c6324ba19d
Alexander Bazo <alexanderbazo@googlemail.com>
2019-04-23 13:22:45 +0200

    Add comments

--- resources/js/Task.js
+++ resources/js/Task.js
@@ -1,14 +1,35 @@
 /* eslint-env browser */
 
+/**
+ * Achtung: Javascript kennt das Schlüsselwort class. Es kann verwendet werden, um auf eine einfache Art und Weise
+ * Objekt-Prototypen zu definieren. Die Konstrukte haben nur wenig mit dem Gemeinsam, was Sie aus Java als Klasse kennen.
+ * Vgl. Classes [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes]
+ * Vgl. "Introducing JavaScript objects" [https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects]
+ */
+
 class Task {
 
+  /**
+   * Constructor function, wird aufgerufen, wenn mittels "new Task()" ein neues Task-Objekt erstellt wird
+   */
   constructor(description, id, completed) {
+    /**
+     * Das this-Schlüsselwort verweist in Javascript immer auf den Kontext, in dem eine Funktion ausgeführt wird und
+     * unterscheidet sich grundlegend von dem bekannten "this" aus Java, das auf eine Instanz verweist. In diesem Fall, 
+     * verweist this auf ein neues, leeres Objekt, für das die definierte constructor function aufgerufen wurde.
+     *
+     * Eigenschaften können in Javascript jederzeit zu Objekten hinzugefügt werden
+     */
     this.description = description || "New Task";
     this.id = id || Date.now().toString();
     this.completed = completed || false;
     this.listeners = {};
   }
   
+  /** 
+   * Funktionen und Eigenschaften von Objekten sind in Javascript grundsätzlich "öffentlich", 
+   * Zugriffsmodifikatoren existieren ebenso wenig wie Rückgabe- oder Parametertypen.
+   */
   setDescription(description) {
     this.description = description;
   }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,46 @@
 /*eslint-env browser */
 
+/**
+ * Die notwendigen Klassen (Achtung, der "Klassen-Begriff" ist in Javascript
+ * mit Vorsicht zu genießen) werden aus den separaten Dateien importiert um in
+ * diesem Modul verwendet zu werden.
+ */
 import Task from "./Task.js";
 import TaskView from "./TaskView.js";
 
+/** 
+ * Map [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map]
+ * zum Speichern der einzelnen Task. Später wird die ID der Tasks als Schlüssel verwendet.
+ *
+ * Variablen werden in Javascript dynamsich typsiert!
+ */
 var taskMap = new Map(),
+/**
+ * Referenziert das HTML-Element (<ul>), in dem die einzelnen Aufgaben als Listenelemente
+ * (<li>) angezeigt werden.
+ */ 
   taskListEl;
 
+/**
+ * Initalisiert die Anwendung:
+ * - Selektion der notwendigen UI-Elemente aus dem DOM
+ * - Registrieren von Listener auf die Interaktionsereignisse, die der Nutzer im UI auslösen kann
+ *
+ * Vgl: Programmatische Repräsentation und Verwendung von Views in Android
+ */
 function init() {
+  /**
+   * Die Variable document beeinhaltet ein Javascript-Objekt, das den DOM repräsentiert
+   * Die Methode querySelector erlaubt die Selektion von Elementen anhand der bekannten CSS-Selektoren
+   */
   let addTaskButton = document.querySelector(".button.new-task"),
     clearListButton = document.querySelector(".button.clear-list");
   taskListEl = document.querySelector(".task-list");
+  /** 
+   * Die addEventListener-Methode erlaubt das Zuweisen einer Callback-Methode zu einem Ereignis:
+   * Wird im Observable (hier: addTaskButton) der Event "click" asugelöst (Mausklick auf das Element), wird
+   * die registrierte Callback-Methode (hier: addTask) aufgerufen.
+   */ 
   addTaskButton.addEventListener("click", addTask);
   clearListButton.addEventListener("click", clearList);
 }
@@ -18,6 +49,10 @@ function addTask() {
   let newTask = new Task(),
     taskView = new TaskView(newTask);
   taskMap.set(newTask.id, newTask);
+  /**
+   * Die appendChild Methode fügt ein neues Element in den DOM ein 
+   * (als Kindelement des angegebenen "parent", hier: taskListEl)
+   */
   taskListEl.appendChild(taskView.getElement());
   taskView.focus();
 }
@@ -31,4 +66,5 @@ function clearList() {
   }
 }
 
+// Aufruf der init-Funktion, nachdem die Datei vollständig eingelesen und interpretiert wurde
 init();
